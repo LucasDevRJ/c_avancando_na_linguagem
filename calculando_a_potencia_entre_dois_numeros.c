7d6c051847bf1019f7acc1ad11a185ecce8e669e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-01-17 20:23:01 -0300

    Programa que calcula potencia com parâmetros

---
+++ calculando_a_potencia_entre_dois_numeros.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+double potencia(int a, int b) {
+	double c = pow(a, b);
+	return c;
+}
+
+int main() {
+	int a, b;
+
+	printf("Digite o valor de a: ");
+	scanf("%d", &a);
+
+	printf("Digite o valor de b: ");
+	scanf("%d", &b);
+
+	double c = potencia(a, b);
+
+	printf("%d elevado a %d equivale a %.1lf", a, b, c);
+}
